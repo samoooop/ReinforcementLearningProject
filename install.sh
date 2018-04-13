09f782f48d7f64fd9fb26bd3f03ea147842f6353
Trairat Punyawatthananukool <samoooop@gmail.com>
2018-04-13 13:44:10 +0700

    update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-sudo apt-get install python3-pip git zlib1g-dev cmake swig
+sudo apt-get install python3-pip git zlib1g-dev cmake swig git mpich libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev
 pip3 install virtualenv
 cd ~
 mkdir .pythonenv
 cd .pythonenv
 virtualenv .
 echo 'source ~/.pythonenv/bin/activate' >> ~/.bashrc
-pip3 install 'gym[all]' opencv-pyhton tensorflow-gpu
+pip3 install gym opencv-python tensorflow-gpu scikit-image
 cd ~/Documents
 git clone https://github.com/openai/baselines
 cd baselines
